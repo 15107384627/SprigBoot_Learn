4e068769894dff9bc1df0850682cfd59ba8f1d4f
15107384627 <shikuifaith@qq.com>
2018-11-16 12:47:54 +0800

    第一次提交

---
+++ sk-spring-soring-starter-master/src/main/java/com/sk/mapper/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.sk.mapper;
+
+import com.sk.pojo.SysUser;
+import com.sk.utils.MyMapper;
+
+public interface SysUserMapper extends MyMapper<SysUser> {
+}
---
+++ sk-spring-soring-starter-master/src/main/java/com/sk/pojo/UserContoller.java
@@ -0,0 +1,45 @@
+package com.sk.pojo;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sk.pojo.IMoocJSONResult;
+import com.sk.pojo.User;
+
+//@Controller
+@RestController		// @RestController = @Controller + @ResponseBody
+@RequestMapping("/user")
+public class UserContoller {
+
+	@RequestMapping("/getUser")
+//	@ResponseBody
+	public User getUser() {
+		
+		User u = new User();
+		u.setName("imooc2");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("imooc2");
+		u.setDesc("hello imooc2~~");
+		
+		return u;
+	}
+	
+	@RequestMapping("/getUserJson")
+//	@ResponseBody
+	public IMoocJSONResult getUserJson() {
+		
+		User u = new User();
+		u.setName("imooc");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("imooc");
+		u.setDesc("hello imooc~~hello imooc~~");
+		
+		return IMoocJSONResult.ok(u);
+	}
+}
